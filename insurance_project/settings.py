806d4c08fd9257fc53d13712f7e000a8e2eb3f0a
Hallcuon <Halcuon4c21@gmail.com>
2025-05-02 19:11:35 +0300

    Update: Додано взаємодію з Front-End частиною

--- insurance_project/settings.py
+++ insurance_project/settings.py
@@ -40,11 +40,14 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'insurance',
+    'corsheaders',
 ]
 
 AUTH_USER_MODEL = 'insurance.User'
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,6 +55,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
 ]
 
 ROOT_URLCONF = 'insurance_project.urls'
